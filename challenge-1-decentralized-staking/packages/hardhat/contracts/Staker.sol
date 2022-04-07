0c08734a4919e8d6f58f415e06b4959d191cb3ce
Cameron Armstrong <Camarmstr@gmail.com>
2022-04-07 16:17:42 -0400

    Challenge 1 Updates

--- challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
+++ challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
@@ -12,31 +12,54 @@ contract Staker {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
   mapping (address => uint256) public balances;
   uint256 public constant threshold = 1 ether;
+  uint256 public deadline = block.timestamp + 72 hours;
+  bool public executeCheck = true;
+  bool public openForWithdraw = false;
+  bool public isComplete = false;
+
   event Stake(address indexed _sender, uint256 _amount);
+
+  modifier notCompleted {
+    require(!exampleExternalContract.completed(), "Already Completed");
+    _;
+  }
+
+  receive() external payable {
+    this.stake();
+    balances[msg.sender] += msg.value;
+  }
 
   function stake() public payable {
     balances[msg.sender] += msg.value;
     emit Stake(msg.sender, msg.value);
   }
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+  function execute() public notCompleted {
+    require(executeCheck, "Already Called");
+    executeCheck = false;
+    if (address(this).balance >= threshold && this.timeLeft()==0) {
+      exampleExternalContract.complete{value: address(this).balance}();
+      balances[msg.sender] = 0;
+    }
+    openForWithdraw = true;
+  }
+
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    }
+    return deadline - block.timestamp;
+  }
 
 
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
-
-
-  // Add a `withdraw()` function to let users withdraw their balance
-
-
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
-
-
-  // Add the `receive()` special function that receives eth and calls stake()
-
+  function withdraw() public notCompleted {
+    require(openForWithdraw,"Not Executed Yet");
+    uint amount = balances[msg.sender];
+    balances[msg.sender] = 0;
+    (bool sent, bytes memory data) = msg.sender.call{value: amount}("");
+    require(sent, "Failed to Withdraw");
+  }
 
 }
